7feba98d6e1212e529af2e0794f431c173db533c
Raul Santamaria <raulsantamariao@hotmail.com>
2024-10-22 22:33:11 +0000

    modified:   ChatWoot/integration.definition.ts

--- ChatWoot/integration.definition.ts
+++ ChatWoot/integration.definition.ts
@@ -5,7 +5,7 @@ import { integrationName } from './package.json'
 
 export default new IntegrationDefinition({
   name: integrationName,
-  version: '4.0.1',
+  version: '4.0.2',
   readme: 'hub.md',
   title: 'Chatwoot',
   description: 'Chatwoot Integration for live agent handoff',
